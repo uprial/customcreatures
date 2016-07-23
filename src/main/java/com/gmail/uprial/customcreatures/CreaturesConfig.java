e4518ef74f1f6613486784ad5a1b23257454a759
Ilya Roslyakov <uprial@gmail.com>
2016-07-23 14:45:22 -0700

    Define local to case changes

--- src/main/java/com/gmail/uprial/customcreatures/CreaturesConfig.java
+++ src/main/java/com/gmail/uprial/customcreatures/CreaturesConfig.java
@@ -1,17 +1,14 @@
 package com.gmail.uprial.customcreatures;
 
 import com.gmail.uprial.customcreatures.common.CustomLogger;
-import com.gmail.uprial.customcreatures.config.InvalidConfigException;
 import com.gmail.uprial.customcreatures.config.ConfigReaderSimple;
+import com.gmail.uprial.customcreatures.config.InvalidConfigException;
 import com.gmail.uprial.customcreatures.schema.HItem;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.gmail.uprial.customcreatures.config.ConfigReaderLists.getKey;
 
@@ -42,13 +39,14 @@ public class CreaturesConfig {
 
         for(int i = 0; i < handlersConfig.size(); i++) {
             String key = getKey(handlersConfig.get(i), "'handlers'", i);
-            if (keys.containsKey(key.toLowerCase())) {
+            String keyLC = key.toLowerCase(Locale.getDefault());
+            if (keys.containsKey(keyLC)) {
                 throw new InvalidConfigException(String.format("Key '%s' in 'handlers' is not unique", key));
             }
             if (config.get(key) == null) {
                 throw new InvalidConfigException(String.format("Null definition of handler '%s' at pos %d", key, i));
             }
-            keys.put(key.toLowerCase(), 1);
+            keys.put(keyLC, 1);
 
             try {
                 handlers.add(HItem.getFromConfig(config, customLogger, key));
