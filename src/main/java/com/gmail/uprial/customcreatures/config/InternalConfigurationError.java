5acf903d8209f1d02ce06e8f2561118f2bd6bee4
Ilya Roslyakov <uprial@gmail.com>
2016-07-23 15:14:18 -0700

    noinspection ExceptionClassNameDoesntEndWithException

--- src/main/java/com/gmail/uprial/customcreatures/config/InternalConfigurationError.java
+++ src/main/java/com/gmail/uprial/customcreatures/config/InternalConfigurationError.java
@@ -1,5 +1,6 @@
 package com.gmail.uprial.customcreatures.config;
 
+@SuppressWarnings("ExceptionClassNameDoesntEndWithException")
 public class InternalConfigurationError extends RuntimeException {
     public InternalConfigurationError(String message) {
         super(message);
