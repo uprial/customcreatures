fa832643bc0c87041dbe2fe66c9a99abcde2e289
Ilya Roslyakov <uprial@gmail.com>
2016-07-24 02:36:54 -0700

    Fixed a bug with durability configuration

--- src/main/java/com/gmail/uprial/customcreatures/schema/HItemDurability.java
+++ src/main/java/com/gmail/uprial/customcreatures/schema/HItemDurability.java
@@ -28,7 +28,7 @@ public final class HItemDurability {
             customLogger.debug(String.format("Handle %s: set durability of %s of %s to %d",
                     title, material, format(entity), itemDurability));
         }
-        itemStack.setDurability((short)Math.round((1.0 * material.getMaxDurability() * itemDurability) / MAX_PERCENT));
+        itemStack.setDurability((short)Math.round((1.0 * material.getMaxDurability() * (MAX_PERCENT - itemDurability)) / MAX_PERCENT));
     }
 
     public static HItemDurability getFromConfig(FileConfiguration config, CustomLogger customLogger, String key, String title) throws InvalidConfigException {
