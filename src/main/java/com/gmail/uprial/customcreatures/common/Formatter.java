602a3da470e942b618a23434c1768b7069175760
Ilya Roslyakov <uprial@gmail.com>
2019-07-08 20:26:35 +0300

    Display nulls properly

--- src/main/java/com/gmail/uprial/customcreatures/common/Formatter.java
+++ src/main/java/com/gmail/uprial/customcreatures/common/Formatter.java
@@ -7,6 +7,9 @@ import org.bukkit.util.Vector;
 
 public final class Formatter {
     public static String format(Entity entity) {
+        if(entity == null) {
+            return "null";
+        }
         Location location = entity.getLocation();
         return String.format("%s[world: %s, x: %.0f, y: %.0f, z: %.0f]",
                 entity.getType().toString(),
@@ -15,11 +18,17 @@ public final class Formatter {
     }
 
     public static String format(PotionEffect effect) {
+        if(effect == null) {
+            return "null";
+        }
         return String.format("%s[strength:%d, duration:%d]",
                 effect.getType().getName(), effect.getAmplifier(), effect.getDuration());
     }
 
     public static String format(Vector vector) {
+        if(vector == null) {
+            return "null";
+        }
         return String.format("[x: %.2f, y: %.2f, z: %.2f, len: %.2f]",
                 vector.getX(), vector.getY(), vector.getZ(), vector.length());
     }
