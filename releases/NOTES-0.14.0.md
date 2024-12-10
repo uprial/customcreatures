0b65a8ed784bf08c78ac83818740b2c2b5060962
Ilya Roslyakov <i_roslyakov@wargaming.net>
2024-12-10 22:35:17 +0200

    Fixed handling of pre-generated entities in structures, like Evokers and Vindicators in woodland mansions

--- releases/NOTES-0.14.0.md
+++ releases/NOTES-0.14.0.md
@@ -3,3 +3,5 @@
 * 10% of Vexes are spawned together with illusioners.
 * 100% of Evokers and Illusioners have a large jump and movement bonus, fire resistance and regeneration, in NETHERITE equipment with maybe top protection and thorns and random support enhancements, a top-enchanted bow. May drop an enchanted golden apple, a netherite scrap, a book of mending, a totem of undying, and a diamond pickaxe.
 * Added 'apply' command
+# Bug fixes
+* Fixed handling of pre-generated entities in structures, like Evokers and Vindicators in woodland mansions. 
--- src/main/java/com/gmail/uprial/customcreatures/listeners/CustomCreaturesSpawnEventListener.java
+++ src/main/java/com/gmail/uprial/customcreatures/listeners/CustomCreaturesSpawnEventListener.java
@@ -3,15 +3,21 @@ package com.gmail.uprial.customcreatures.listeners;
 import com.gmail.uprial.customcreatures.CreaturesConfig;
 import com.gmail.uprial.customcreatures.CustomCreatures;
 import com.gmail.uprial.customcreatures.common.CustomLogger;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.world.EntitiesLoadEvent;
 import org.bukkit.inventory.ItemStack;
 
+import static com.gmail.uprial.customcreatures.common.PersistenceHelper.addPersistentMetadataFlag;
+import static com.gmail.uprial.customcreatures.common.PersistenceHelper.containsPersistentMetadataFlag;
+
 public class CustomCreaturesSpawnEventListener extends AbstractCustomCreaturesEventListener {
+    private static final String MK_HANDLED = "handled";
 
     public CustomCreaturesSpawnEventListener(CustomCreatures plugin, CustomLogger customLogger) {
         super(plugin, customLogger);
@@ -21,7 +27,7 @@ public class CustomCreaturesSpawnEventListener extends AbstractCustomCreaturesEv
     @EventHandler(priority = EventPriority.NORMAL)
     public void onCreatureSpawn(CreatureSpawnEvent event) {
         if (!event.isCancelled()) {
-            handleSpawn(event.getEntity(), event.getSpawnReason());
+            handleSpawnOncePerEntity(event.getEntity(), event.getSpawnReason());
         }
     }
 
@@ -34,7 +40,32 @@ public class CustomCreaturesSpawnEventListener extends AbstractCustomCreaturesEv
                 return;
             }
         }
+        /*
+            According to https://hub.spigotmc.org/javadocs/spigot/org/bukkit/event/entity/CreatureSpawnEvent.SpawnReason.html,
+            DEFAULT - when an entity is missing a SpawnReason.
+         */
         handleSpawn(event.getPlayer(), SpawnReason.DEFAULT);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onEntitiesLoadEvent(EntitiesLoadEvent event) {
+        for(Entity entity : event.getEntities()) {
+            if(entity instanceof LivingEntity) {
+                /*
+                    According to https://hub.spigotmc.org/javadocs/spigot/org/bukkit/event/entity/CreatureSpawnEvent.SpawnReason.html,
+                    NATURAL - when something spawns from natural means.
+                 */
+                handleSpawnOncePerEntity((LivingEntity)entity, SpawnReason.NATURAL);
+            }
+        };
+    }
+
+    private void handleSpawnOncePerEntity(LivingEntity entity, SpawnReason spawnReason) {
+        if(!containsPersistentMetadataFlag(plugin, entity, MK_HANDLED)) {
+            handleSpawn(entity, spawnReason);
+
+            addPersistentMetadataFlag(plugin, entity, MK_HANDLED);
+        }
     }
 
     private void handleSpawn(LivingEntity entity, SpawnReason spawnReason) {
